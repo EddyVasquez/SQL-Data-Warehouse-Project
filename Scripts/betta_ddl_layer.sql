cf4479248de439fbdb29eb78c6c3843ae6ff175e
EddyVasquez <eddymars93@gmail.com>
2025-12-10 23:13:14 -0600

    Update betta_ddl_layer.sql

--- Scripts/betta_ddl_layer.sql
+++ Scripts/betta_ddl_layer.sql
@@ -1,1 +1,18 @@
+/*
+=========================================================
 
+DDL Script: Create Betta Tables
+
+=========================================================
+
+Script Purpose:
+
+The script creates tables in the 'betta' schema, dropping existing tables if they already exist.
+Run this script to re-define the DDL structure of 'betta' Tables.
+
+=========================================================
+*/
+
+
+
+  
