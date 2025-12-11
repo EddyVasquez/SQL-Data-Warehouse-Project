ad772cf6f1d28a7bc7f68e43a2bea1e03f0fa58a
EddyVasquez <eddymars93@gmail.com>
2025-12-10 23:45:49 -0600

    Update betta_proc_load.sql

--- Scripts/Betta/betta_proc_load.sql
+++ Scripts/Betta/betta_proc_load.sql
@@ -1,1 +1,26 @@
+/*
+=========================================================
+
+Stored Procedure: Load Bronze Layer (Source -> Betta)
+
+=========================================================
+
+Script Purpose:
+
+This stored procedure loads data into the 'betta' schema from external .csv files.
+It perfroms the following actions:
+
+-Truncates the betta tables before loading data.
+-Uses the 'BULK INSERT' command to load data from .csv files to betta tables.
+
+Parameters:
+
+None. This stored procedure does not accept any parameters or return any values. 
+
+Usage Example:
+
+  EXEC betta.load_betta;
+
+=========================================================
+*/
 
