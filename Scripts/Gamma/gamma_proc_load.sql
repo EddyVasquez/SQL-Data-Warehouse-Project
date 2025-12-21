bed406e94a396c9595908789a51d64e531ec3917
EddyVasquez <247858821+EddyVasquez@users.noreply.github.com>
2025-12-21 14:12:24 -0600

    Update gamma_proc_load.sql

--- Scripts/Gamma/gamma_proc_load.sql
+++ Scripts/Gamma/gamma_proc_load.sql
@@ -1,1 +1,23 @@
+/*
+=========================================================
 
+Stored Procedure: Load Gamma Layer (Source -> Beta)
+
+=========================================================
+
+Script Purpose:
+
+This stored procedure loads data into the 'gamma' schema from beta layer raw data
+
+-Truncates the gamma tables before loading data.
+
+Parameters:
+
+None. This stored procedure does not accept any parameters or return any values. 
+
+Usage Example:
+
+  EXEC gama.load_gama;
+
+=========================================================
+*/
